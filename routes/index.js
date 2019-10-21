ef90c71f4683bca65ab4d296475f9279becdbd8b
Himanshu Chanchad <42400887+himanshuchanchad@users.noreply.github.com>
2019-10-21 17:36:46 +0530

    signup added

--- routes/index.js
+++ routes/index.js
@@ -32,11 +32,26 @@ router.post("/login",async (req,res,next)=>{
 });
 router.get("/signup",(req,res,next)=>{
     res.render('signup',{
+        signupError :false,
     });
 });
-router.post("/signup",(req,res,next)=>{
-    // res.render('signup',{
-    // });
+router.post("/signup",async (req,res,next)=>{
+    const sql =`insert into user(sap_id,password,firstname,middlename,lastname,department,phoneno,email) values(${req.body.sap_id},'${req.body.password}','${req.body.firstname}','${req.body.middlename}','${req.body.lastname}','${req.body.department}',${req.body.phoneno},'${req.body.email}')`;
+    try {
+        const [rows,fields]=await db.execute(sql);
+        const user = rows[0];
+        console.log(user);
+        res.render('home',{
+            }
+            );
+
+    } catch (error) {
+        res.render('signup',
+        {
+        signupError :true,
+        });
+        console.log(error);
+    }
 });
 
 router.get("/home",(req,res,next)=>{
