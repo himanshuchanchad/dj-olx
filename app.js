1589f63c29777ea095d1d4ba1e770177433a7eea
Himanshu Chanchad <42400887+himanshuchanchad@users.noreply.github.com>
2019-10-23 14:17:02 +0530

    complete

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express= require("express");
 const app=express();
-// const adminRoutes=require("./routes/admin");
 const indexRoutes=require("./routes/index");
 const bodyParser = require('body-parser');
 const path =require('path');
+let cookieParser = require('cookie-parser'); 
 
 app.set('view engine', 'ejs');
 app.set('views','templates');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'static')));
-
+app.use(cookieParser());
 app.use('/',indexRoutes);
-// app.use('/admin',adminRoutes);
 
 app.use((req,res,next)=>{
 res.status(404).sendFile(path.join(__dirname,'templates','404.html'));
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,6 @@ const storage = multer.diskStorage({
         cb(null, 'static/images');
     },
     filename: (req, file, cb) => {
-        // let name = makeid(10)+'.'+file.mimetype.split('/')[1];
         imageURLNew =  file.originalname;
         cb(null, file.originalname);
     }
@@ -52,6 +51,7 @@ router.post("/login",async (req,res,next)=>{
         );
     }
     else{
+        res.cookie("sap_id", req.body.username); 
         res.redirect("/home");
     }
     } catch (error) {
@@ -66,9 +66,30 @@ router.get("/signup",(req,res,next)=>{
 });
 
 router.post("/signup",async (req,res,next)=>{
-    const sql =`insert into user(sap_id,password,firstname,middlename,lastname,department,phoneno,email) values(${req.body.sap_id},'${req.body.password}','${req.body.firstname}','${req.body.middlename}','${req.body.lastname}','${req.body.department}',${req.body.phoneno},'${req.body.email}')`;
+    if(req.password==null){
+        let sql1 =`insert into user(sap_id,password,firstname,middlename,lastname,department,phoneno,email) values(${req.body.sap_id},'pass@123','${req.body.firstname}','${req.body.middlename}','${req.body.lastname}','${req.body.department}',${req.body.phoneno},'${req.body.email}')`;
     try {
-        const [rows,fields]=await db.execute(sql);
+        const [rows,fields]=await db.execute(sql1);
+        const user = rows[0];
+        await db.execute(`insert into buyer(sap_id ) values(${req.body.sap_id})`);
+        await db.execute(`insert into seller(sap_id ) values(${req.body.sap_id})`);
+        res.render('index',{
+            loginError : false,
+        });
+    } catch (error) {
+        res.render('signup',
+        {
+        signupError :true,
+        });
+        console.log(error);
+    }
+
+    }
+    else{
+
+    let sql1 =`insert into user(sap_id,password,firstname,middlename,lastname,department,phoneno,email) values(${req.body.sap_id},'${req.body.password}','${req.body.firstname}','${req.body.middlename}','${req.body.lastname}','${req.body.department}',${req.body.phoneno},'${req.body.email}')`;
+    try {
+        const [rows,fields]=await db.execute(sql1);
         const user = rows[0];
         res.render('index',{
             loginError : false,
@@ -80,19 +101,21 @@ router.post("/signup",async (req,res,next)=>{
         });
         console.log(error);
     }
+}
 });
 
 router.get("/home",async (req,res,next)=>{
     let query=req.query.search;
     let sql;
+    
     if(query)
     {
-         sql =`select *  from items where title like '%${query}%' or description like '%${query}%'`;
+         sql =`select *  from items where active =1  and  (title like '%${query}%' or description like '%${query}%' )`;
     }
     else{
-     sql=`select * from items`;
+     sql=`select * from items where active =1`;
     }
-    console.log(sql);
+    // console.log(sql);
     let items;
     try {
         const [rows,fields]=await db.execute(sql);
@@ -101,7 +124,6 @@ router.get("/home",async (req,res,next)=>{
     } catch (error) {
         
     }
-    
     res.render('home',{
         items : items ,
     });
@@ -115,7 +137,7 @@ router.get("/add-items",(req,res,next)=>{
 });
 
 router.post("/add-items",upload.single('image'),async (req,res,next)=>{
-    const sql=`insert into items(title,description,image,price,conditions) values('${req.body.title}','${req.body.description}','${imageURLNew}',${req.body.price},'${req.body.condition}')`;
+    const sql=`insert into items(sap_id,title,description,image,price,conditions) values('${req.cookies.sap_id}','${req.body.title}','${req.body.description}','${imageURLNew}',${req.body.price},'${req.body.condition}')`;
     try {
         const [rows,fields]=await db.execute(sql);
         res.redirect('/home');
@@ -124,6 +146,63 @@ router.post("/add-items",upload.single('image'),async (req,res,next)=>{
         res.render('add-items',{
         });
     }
+});
+router.get("/logout",(req,res,next)=>{
+    res.clearCookie("sap_id");
+    res.redirect("/");
+});
+router.get("/manage-items", async (req,res,next)=>{
+    let sql=`select * from items where active =1 and sap_id =${req.cookies.sap_id}`;
+    let sql1= `select * from  user  where sap_id=${req.cookies.sap_id}`;
+    console.log(sql);
+    
+    let items;
+    try {
+        const [rows,fields] =await db.execute(sql);
+        items=rows
+        let [row,field] = await db.execute(sql1);
+        user = row[0]
+        res.render('manage-items',{
+            items: items,
+            user : user,
+        });
+    } catch (error) {
+        console.log(error);
+        
+    }
+    res.redirect("/home");
+});
+
+router.get("/deactivate-items",async (req,res,next)=>{
+    let query=req.query.search;
+    let sql=`update items set active=0 where items_id=${query}`;
+    try {
+        let [row,field] = await db.execute(sql);
+        res.redirect("/manage-items");
+    } catch (error) {
+        
+    }
+    res.redirect("/home");
+});
+router.get("/items",async (req,res,next)=>{
+    let query=req.query.search;
+    let sql= `select * from  user  where sap_id =(select sap_id from items where items_id =${query})`;
+    const sql2 = `select * from items where active=1 and  items_id =${query}`;
+    let item,user;
+    try {
+        let [rows,fields]=await db.execute(sql2);
+         item = rows[0];
+         let [row,field]=await db.execute(sql);
+         user = row[0];
+         console.log(item,user);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    res.render('items',{
+        item: item,
+        user : user ,
+    });
 });
 
 
